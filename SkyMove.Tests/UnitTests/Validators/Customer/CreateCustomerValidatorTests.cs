ea8bf6f572d19c989e00a8ed71a4848ed3c33a69
eriksantos617@gmail.com <eriksantos617@gmail.com>
2026-01-22 11:39:03 -0300

    feat: criação do projeto de testes / primeiros testes unitários

---
+++ SkyMove.Tests/UnitTests/Validators/Customer/CreateCustomerValidatorTests.cs
@@ -0,0 +1,114 @@
+﻿using FluentValidation.TestHelper;
+using SkyMove.Application.Dtos.Customer;
+using SkyMove.Application.Validators.Customer;
+
+namespace SkyMove.Tests.UnitTests.Validators.Customer
+{
+    public class CreateCustomerValidatorTests
+    {
+        private readonly CreateCustomerValidator _validator;
+
+        public CreateCustomerValidatorTests()
+        {
+            _validator = new();
+        }
+
+        #region MemberData
+
+        public static IEnumerable<object[]> InvalidFirstNames => [[""], ["Ab"], [new string('a', 101)]];
+
+        public static IEnumerable<object[]> InvalidLastNames => [[""], ["Ab"], [new string('a', 101)]];
+
+        public static IEnumerable<object[]> InvalidCpfs => [[""], ["123"], ["123456789012"]];
+
+        public static IEnumerable<object[]> InvalidDdds => [[""], ["1"], ["123"]];
+
+        public static IEnumerable<object[]> InvalidPhoneNumbers => [[""], ["12345678"], ["1234567890"]];
+
+        public static IEnumerable<object[]> InvalidEmails => [[""], ["invalidemail"], ["invalid@"]];
+
+        public static IEnumerable<object[]> InvalidDateOfBirths => [[DateTime.Today.AddYears(-17)], [DateTime.Today.AddYears(-1)]];
+
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(InvalidFirstNames))]
+        public void FirstName_Should_Have_Error_When_Invalid(string firstName)
+        {
+            var model = new CreateCustomerRequest { FirstName = firstName };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.FirstName);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidLastNames))]
+        public void LastName_Should_Have_Error_When_Invalid(string lastName)
+        {
+            var model = new CreateCustomerRequest { LastName = lastName };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.LastName);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidCpfs))]
+        public void Cpf_Should_Have_Error_When_Invalid(string cpf)
+        {
+            var model = new CreateCustomerRequest { Cpf = cpf };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Cpf);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidDdds))]
+        public void Ddd_Should_Have_Error_When_Invalid(string ddd)
+        {
+            var model = new CreateCustomerRequest { Ddd = ddd };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Ddd);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidPhoneNumbers))]
+        public void PhoneNumber_Should_Have_Error_When_Invalid(string phoneNumber)
+        {
+            var model = new CreateCustomerRequest { PhoneNumber = phoneNumber };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.PhoneNumber);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidEmails))]
+        public void Email_Should_Have_Error_When_Invalid(string email)
+        {
+            var model = new CreateCustomerRequest { Email = email };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Email);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidDateOfBirths))]
+        public void DateOfBirth_Should_Have_Error_When_Under18(DateTime dateOfBirth)
+        {
+            var model = new CreateCustomerRequest { DateOfBirth = dateOfBirth };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.DateOfBirth);
+        }
+
+        [Fact]
+        public void Should_Not_Have_Error_For_Valid_Model()
+        {
+            var model = new CreateCustomerRequest
+            {
+                FirstName = "John",
+                LastName = "Doe",
+                Cpf = "12345678901",
+                Ddd = "11",
+                PhoneNumber = "912345678",
+                DateOfBirth = DateTime.Today.AddYears(-25),
+                Email = "john.doe@example.com"
+            };
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
---
+++ SkyMove.Tests/UnitTests/Validators/Vehicle/CreateVehicleValidatorTests.cs
@@ -0,0 +1,100 @@
+﻿using FluentValidation.TestHelper;
+using SkyMove.Application.Dtos.Vehicle;
+using SkyMove.Application.Validators.Vehicle;
+using SkyMove.Domain.Enums;
+
+namespace SkyMove.Tests.UnitTests.Validators.Vehicle
+{
+    public class CreateVehicleValidatorTests
+    {
+        private readonly CreateVehicleValidator _validator;
+
+        public CreateVehicleValidatorTests()
+        {
+            _validator = new();
+        }
+
+        #region MemberData
+
+        public static IEnumerable<object[]> InvalidShortTexts => [[""], ["a"], ["ab"]];
+        public static IEnumerable<object[]> InvalidKilometers => [[-1m], [5001m], [10000m]];
+        public static IEnumerable<object[]> InvalidYears => [[DateTime.Today.Year - 3], [DateTime.Today.Year - 10]];
+        public static IEnumerable<object[]> InvalidHorsePower => [[30], [-50]];
+
+        #endregion
+
+        [Theory]
+        [MemberData(nameof(InvalidShortTexts))]
+        public void Brand_Should_Have_Error_When_Less_Than_3_Characters(string brand)
+        {
+            var model = new CreateVehicleRequest { Brand = brand };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Brand);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidShortTexts))]
+        public void Model_Should_Have_Error_When_Less_Than_3_Characters(string modelName)
+        {
+            var model = new CreateVehicleRequest { Model = modelName };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Model);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidShortTexts))]
+        public void Version_Should_Have_Error_When_Less_Than_3_Characters(string version)
+        {
+            var model = new CreateVehicleRequest { Version = version };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Version);
+        }
+
+
+        [Theory]
+        [MemberData(nameof(InvalidKilometers))]
+        public void Kilometers_Should_Have_Error_When_Invalid(decimal kilometers)
+        {
+            var model = new CreateVehicleRequest { Kilometers = kilometers };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Kilometers);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidYears))]
+        public void Year_Should_Have_Error_When_Older_Than_2_Years(int year)
+        {
+            var model = new CreateVehicleRequest { Year = year };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.Year);
+        }
+
+        [Theory]
+        [MemberData(nameof(InvalidHorsePower))]
+        public void HorsePower_Should_Have_Error_When_Invalid(int horsePower)
+        {
+            var model = new CreateVehicleRequest { HorsePower = horsePower };
+            var result = _validator.TestValidate(model);
+            result.ShouldHaveValidationErrorFor(x => x.HorsePower);
+        }
+
+
+        [Fact]
+        public void Should_Not_Have_Error_For_Valid_Model()
+        {
+            var model = new CreateVehicleRequest
+            {
+                Brand = "Toyota",
+                Model = "Corolla",
+                Version = "XEI",
+                Year = DateTime.Today.Year - 1,
+                Kilometers = 2500,
+                HorsePower = 177,
+                Color = Color.Black
+            };
+
+            var result = _validator.TestValidate(model);
+            result.ShouldNotHaveAnyValidationErrors();
+        }
+    }
+}
